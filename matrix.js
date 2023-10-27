87d217596e40b8bb7044a6a9119ce6c575e4e73d
rkeenan <ross.keenan@protonmail.comm>
2023-10-26 21:33:02 -0500

    Now printing licenses and states.

--- matrix.js
+++ matrix.js
@@ -5,6 +5,8 @@ var licenseMatrix = [
 ];
 var requiredDocs = [];
 function nestedForArray(stateName, licenseName, arr) {
+    var loopState = "";
+    var loopLicense = "";
     for (var i = 0; i < arr.length; i++) {
         var state = arr[i];
         for (var j = 0; j < state.length; j++) {
@@ -12,11 +14,24 @@ function nestedForArray(stateName, licenseName, arr) {
             //console.log(state);
             if (Array.isArray(arr[i][j])) {
                 //console.log(state);
+                //console.log(license);
                 var docs = arr[i][j];
-                requiredDocs.push(docs);
+                Array.isArray(docs) ? requiredDocs.push.apply(requiredDocs, docs) : [];
+            }
+            else if (!Array.isArray(arr[i][j]) && j === 0) {
+                loopState = arr[i][j];
+                console.log("loopState: ".concat(loopState));
+            }
+            else if (!Array.isArray(arr[i][j]) && j === 1) {
+                loopLicense = arr[i][j];
+                console.log("loopLicense: ".concat(loopLicense));
+                //const flattenedArray = arr.flat();
+                //loopLicense = arr.flat();
+                //loopLicense = arr[j];
+                //console.log(flattenedArray);
             }
         }
     }
-    console.log(requiredDocs);
+    //console.log(requiredDocs);
 }
 nestedForArray("Hello", "World", licenseMatrix);
--- matrix.ts
+++ matrix.ts
@@ -6,7 +6,13 @@ const licenseMatrix: [string, string, string[]][] = [
 
 const requiredDocs: string[] = [];
 
-function nestedForArray(stateName, licenseName, arr) {
+function nestedForArray(
+  stateName: string,
+  licenseName: string,
+  arr: [string, string, string[]][]
+) {
+  let loopState = "";
+  let loopLicense = "";
   for (var i = 0; i < arr.length; i++) {
     var state = arr[i];
     for (var j = 0; j < state.length; j++) {
@@ -14,12 +20,23 @@ function nestedForArray(stateName, licenseName, arr) {
       //console.log(state);
       if (Array.isArray(arr[i][j])) {
         //console.log(state);
+        //console.log(license);
         var docs = arr[i][j];
-        requiredDocs.push(docs);
+        Array.isArray(docs) ? requiredDocs.push(...docs) : [];
+      } else if (!Array.isArray(arr[i][j]) && j === 0) {
+        loopState = arr[i][j];
+        console.log(`loopState: ${loopState}`);
+      } else if (!Array.isArray(arr[i][j]) && j === 1) {
+        loopLicense = arr[i][j];
+        console.log(`loopLicense: ${loopLicense}`);
+        //const flattenedArray = arr.flat();
+        //loopLicense = arr.flat();
+        //loopLicense = arr[j];
+        //console.log(flattenedArray);
       }
     }
   }
-  console.log(requiredDocs);
+  //console.log(requiredDocs);
 }
 
 nestedForArray("Hello", "World", licenseMatrix);
